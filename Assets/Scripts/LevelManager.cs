71ba8e20ef8d6e5e3cc6733fcfd374099fca8899
Jake <jaketreleaven@gmail.com>
2021-04-09 21:29:23 -0400

    Zombie AI, Spawners and Timer

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class Timer
+{
+    public Timer() { }
+    public Timer(float hours, float minutes, float seconds) 
+    {
+        _hours = hours; _minutes = minutes; _seconds = seconds;
+    }
+    private float _seconds = 0;
+    public float Seconds { get { return _seconds; } }
+    private float _minutes = 0;
+    public float Minutes { get { return _minutes; } }
+    private float _hours = 0;
+    public float Hours { get { return _hours; } }
+
+    public void AddTime(float seconds, float minutes = 0, float hours = 0)
+    {
+        _seconds += seconds;
+        _minutes += minutes;
+        _hours += hours;
+        if (_seconds >= 60)
+        { _seconds -= 60; _minutes += 1; }
+        if (_minutes >= 60)
+        { _minutes -= 60; _hours += 1; }
+    }
+
+    public void RemoveTime(float seconds, float minutes = 0, float hours = 0)
+    {
+        _seconds -= seconds;
+        _minutes -= minutes;
+        _hours -= hours;
+        if (_minutes <= 0 && _hours <= 0 && _seconds < 0)
+        { _seconds = 0; return; }
+
+        if (_seconds <= 0)
+        { _seconds += 60; _minutes -= 1; }
+        if (_minutes <= 0)
+        { _minutes = 60; _hours -= 1; }
+        if (_hours < 0 )
+        { _minutes = 0; _hours = 0; }
+    }
+
+    public string PrintTime()
+    {
+        string seconds = _seconds < 10 ? "0" + ((int)_seconds).ToString() : ((int)_seconds).ToString();
+        string minutes = _minutes < 10 ? "0" + ((int)_minutes).ToString() : ((int)_minutes).ToString();
+
+        return _hours.ToString() + ":" + minutes + ":" + seconds;
+    }
+}
+
+public class LevelManager : Singleton<LevelManager>
+{
+    [Range(1, 10)]
+    public int Difficulty = 1;
+    public Vector3 Timer = new Vector3(0, 5, 0);
+    public TextMeshProUGUI timerText;
+
+    private Timer clock;
+    private bool won;
+
+    private void Start()
+    {
+        won = false;
+        clock = new Timer(Timer.x, Timer.y, Timer.z);
+        SpawnerManager.spawnTime /= Difficulty;
+        SpawnerManager.maxZombies = Difficulty * SpawnerManager.maxZombies;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clock.Seconds <= 0 && clock.Minutes <= 0 && clock.Hours <= 0)
+        {
+            if (!won)
+            //TODO:: GAME WIN!!
+                StartCoroutine(GameWin());
+            return;
+        }
+
+        clock.RemoveTime(Time.deltaTime);
+        timerText.text = clock.PrintTime();
+    }
+
+    IEnumerator GameWin()
+    {
+        timerText.text = "YOU WIN!";
+        won = true;
+        while (isActiveAndEnabled)
+        {
+            yield return new WaitForSeconds(.75f);
+            timerText.enabled = !timerText.enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/Zombie/ZombieController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour, IDamageable, IKillable
+{
+    [SerializeField] float attackSpeed = 2;
+    [SerializeField] float attackRange = 1;
+    [SerializeField] float walkSpeed = 1;
+    [SerializeField] float health = 10;
+
+    private GameObject player;
+    private NavMeshAgent ai;
+    private Animator animator;
+    private static readonly int IsAttackingHash = Animator.StringToHash("isAttacking");
+
+    private bool canAttack = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+        animator = GetComponent<Animator>();
+        ai = GetComponent<NavMeshAgent>();
+        ai.speed = walkSpeed;
+
+        StartCoroutine(UpdatePath());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canAttack && Vector3.Distance(transform.position, player.transform.position) < attackRange)
+        {
+            animator.SetBool(IsAttackingHash, true);
+            //Damage here
+            canAttack = false;
+            StartCoroutine(AttackCooldown());
+        }
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(attackSpeed);
+        canAttack = true;
+    }
+
+    public void StopAttack()
+    {
+        animator.SetBool(IsAttackingHash, false);
+    }
+
+    IEnumerator UpdatePath()
+    {
+        while(isActiveAndEnabled)
+        {
+            yield return new WaitForSeconds(1);
+            //Set Destination
+            ai.SetDestination(player.transform.position);
+            if (!ai.hasPath)
+            {
+                //Idle
+            }
+        }
+    }
+
+    public void Kill()
+    {
+        SpawnerManager.numZombies--;
+    }
+
+    public void Damage(float damageTaken)
+    {
+        health += damageTaken;
+    }
+}
